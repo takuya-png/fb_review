af84da72b1c8bf77c219a2d8e937d455fb85c6aa
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-23 21:58:19 +0900

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'pictures/new'
+  get 'pictures/index'
+  get 'pictures/show'
+  get 'pictures/edit'
+  get 'pictures/confirm'
+  get 'users/new'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
