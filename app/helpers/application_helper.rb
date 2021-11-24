19f822c35b4516e467c76984e0f206637d2ddf28
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-24 12:20:11 +0900

    application_controller作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  before_action :login_required
+  
+  private
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
