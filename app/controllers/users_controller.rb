265023cf78ff2120cce652033900304d01c9def3
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-24 10:26:22 +0900

    users_controller createアクション追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if 
+      @user.save 
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
   def show
   end
 end
