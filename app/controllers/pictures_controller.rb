ac10d8dbf53aeb631e0383433d07a5ff7257c7af
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-24 13:45:44 +0900

    pictures_controllerアクション作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,25 @@
 class PicturesController < ApplicationController
-  def new
-  end
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
   end
 
   def show
@@ -11,6 +28,30 @@ class PicturesController < ApplicationController
   def edit
   end
 
+  def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice:"ブログを削除しました！"
+  end
+
   def confirm
+    @picture = Picture.new(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:title, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
