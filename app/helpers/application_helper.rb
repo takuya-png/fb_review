a119a7a42e51adbc3cf95abe2446eb22977df9fb
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-26 13:47:19 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  protect_from_forgery with: :exception
-  include SessionsHelper
-  before_action :login_required
-  
-  private
-  def login_required
-    redirect_to new_session_path unless current_user
-  end
 end
