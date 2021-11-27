0f76a5023843e2fd3d941f9f8503234da10e51fb
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-27 16:51:10 +0900

    現在ログインしているuserのidを、blogのuser_idカラムに挿入する記述追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -11,6 +11,7 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id 
     if params[:back]
       render :new
     else
@@ -43,6 +44,7 @@ class PicturesController < ApplicationController
 
   def confirm
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id 
     render :new if @picture.invalid?
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if 
-      @user.save 
+    if @user.save 
       redirect_to user_path(@user.id), notice: "ユーザが作成されました！"
     else
       render :new
